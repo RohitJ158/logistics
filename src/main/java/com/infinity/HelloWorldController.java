663865d27387b3f754af8a8b0e93f11f9770aa49
Rohit <rohit-rj.jadhav@broadcom.com>
2022-01-03 11:16:56 +0530

    first commit

---
+++ src/main/java/com/infinity/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.infinity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    String getHelloWorld() {
+        return "Hello, World!";
+    }
+}
